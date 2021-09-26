7ea5c10c623904cf34e0ce585ecbd664320bb5ce
Thomaz <79727201+thomazgg@users.noreply.github.com>
2021-09-26 13:20:30 -0300

    Add files via upload

---
+++ commands/Nsfw/kitsune.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const { MessageEmbed } = require('discord.js')
+const NSFW = require('discord-nsfw')
+const colors = require('./../../colors.json')
+const nsfw = new NSFW();
+module.exports = {
+    name: "kitsune",
+    description: " nsfw cmd!",
+    async run (client, message, args) {
+
+if(message.channel.nsfw) {
+
+const image = await nsfw.kitsune();
+const embed = new Discord.MessageEmbed()
+    .setTitle(`Kitsune Image`)
+    .setColor(colors.uptime)
+    .setImage(image);
+message.channel.send(embed);
+
+} else {
+message.channel.send("💢 canal não é nsfw")
+}
+
+    }
+}
