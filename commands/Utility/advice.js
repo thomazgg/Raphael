ccabfbf639fca9b5949db20a13e55d1ac5b75c65
Thomaz <79727201+thomazgg@users.noreply.github.com>
2021-09-26 13:23:00 -0300

    Create advice.js

---
+++ commands/Utility/advice.js
@@ -0,0 +1,15 @@
+const api = require("srod-v2");
+const Discord = require("discord.js");
+const colors = require('./../../colors.json')
+
+module.exports = {
+  name: "advice",
+  aliases: [],
+  description: "Return A Random Advice!",
+  usage: "Advice",
+  run: async (client, message, args) => {
+    
+    const Data = await api.GetAdvice({ Color: "#39d822" });
+    return message.channel.send(Data);
+  }
+}; 
